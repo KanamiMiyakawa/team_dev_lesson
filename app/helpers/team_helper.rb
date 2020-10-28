6a83f669b92b0686870966da0afa83e7e5d1fbf7
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-28 02:05:06 +0000

    update team show view

--- app/helpers/team_helper.rb
+++ app/helpers/team_helper.rb
@@ -8,4 +8,10 @@ module TeamHelper
       link_to I18n.t('views.button.delete'), team_assign_path(team, assign), method: :delete, class: 'btn btn-sm btn-danger'
     end
   end
+
+  def change_owner_btn_appearence(team,assign)
+    if current_user.id != assign.user_id && current_user == team.owner
+      link_to I18n.t('views.button.change_owner'), change_owner_team_path(id:team.id, new_owner_id:assign.user.id), method: :post, class: 'btn btn-sm btn-danger'
+    end
+  end
 end
